49e9a150a18514ae3c724c43b6e110b8db891c62
Mikhail_Egorkin <mikhail.egorkin.external@zalando.de>
2024-07-21 19:26:48 +0200

    remove gray and white colours

--- src/main/java/org/example/CardAutoCoder.java
+++ src/main/java/org/example/CardAutoCoder.java
@@ -12,7 +12,7 @@ public class CardAutoCoder {
     for (int x = 0; x < subImage.getWidth(); x++) {
       for (int y = 0; y < subImage.getHeight(); y++) {
         Color c = new Color(subImage.getRGB(x, y));
-        if (!c.equals(new Color(255, 255, 255))
+        if (!(c.getRed() > 100 && c.getGreen() > 100 && c.getBlue() > 100)
         && (!c.equals(new Color(120, 120, 120)))) {
           colourSinMatrix.add(c);
         }
@@ -21,3 +21,5 @@ public class CardAutoCoder {
     return "dummy";
   }
 }
+// 7dJc6sAh5s
+// 10s7dKh
