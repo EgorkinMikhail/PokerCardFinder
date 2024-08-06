be845d1d176de0a454c7fbde968c6b55b5d61ec0
Mikhail_Egorkin <mikhail.egorkin.external@zalando.de>
2024-08-06 10:29:28 +0200

    refactoring

--- run.bat
+++ run.bat
@@ -1,20 +1,8 @@
-@echo off
-setlocal
-
+@echo offsetlocal
 set JAR_NAME=PokerCardFinder-1.0-SNAPSHOT.jar
-
 if not exist "%~dp0%JAR_NAME%" (
-    echo Error: %JAR_NAME% not found in the current directory.
-    pause
-    exit /b 1
+    echo Error: %JAR_NAME% not found in the current directory.    exit /b 1
 )
-
-if "%1"=="" (
-    echo Error: No argument provided.
-    echo Usage: run.bat <argument>
-    pause
-    exit /b 1
-)
-
+if "%1"=="" (    echo Error: No argument provided
+    exit /b 1)
 java -jar "%~dp0%JAR_NAME%" %1
-pause
--- src/main/java/org/example/App.java
+++ src/main/java/org/example/App.java
@@ -8,15 +8,23 @@ import java.io.IOException;
 public class App {
 
     public static void main(String[] args) throws IOException {
+        File folder = new File(args[0]);
+        File[] listOfFiles = folder.listFiles();
         Solution solution = new Solution();
-        BufferedImage initialImage = readImageFromPath(args[0]);
-        for (int i = 0; i < 5; i++) {
-            BufferedImage card = solution.getCardByNum(initialImage, i);
-            if (solution.checkIsCardExist(card)) {
-                System.out.printf("%s%s", solution.getValue(card), solution.getSuit(card).charAt(0));
+        for (File file : listOfFiles) {
+            if (file.isFile()) {
+                if (file.getName().substring(file.getName().length() - 3).equalsIgnoreCase("png")) {
+                    BufferedImage initialImage = readImageFromPath(file.getAbsolutePath());
+                    for (int i = 0; i < 5; i++) {
+                        BufferedImage card = solution.getCardByNum(initialImage, i);
+                        if (solution.checkIsCardExist(card)) {
+                            System.out.printf("%s%s", solution.getValue(card), solution.getSuit(card).charAt(0));
+                        }
+                    }
+                }
+                System.out.println();
             }
         }
-        System.out.println();
     }
 
     static BufferedImage readImageFromPath(String filePath) throws IOException {
